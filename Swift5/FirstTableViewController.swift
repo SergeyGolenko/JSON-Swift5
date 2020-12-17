322301f7acaea379990f409d0d709bd92181f060
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-17 18:01:14 +0200

    данные JSON переводятся в новый созданный тип ConvertJson , добавляются в массив и отображаются в таблице

--- Swift5/FirstTableViewController.swift
+++ Swift5/FirstTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class FirstTableViewController: UITableViewController {
     
+    var items : [ConvertJson] = []
+    
     let urlZappy = "https://zappycode.com/api/courses"
     
     func getCources(){
@@ -16,11 +18,21 @@ class FirstTableViewController: UITableViewController {
             var request = URLRequest(url: url)
             request.httpMethod = "GET"
             
-            URLSession.shared.dataTask(with: request) {data,response,error  in
+            URLSession.shared.dataTask(with: request) { [self]data,response,error  in
                 if error != nil {
                     print("There was an error")
                 } else if data != nil {
-                    print(String(data:data!,encoding: .utf8))
+                   // print(String(data:data!,encoding: .utf8))
+                    if let courcesFromJson =  try?  JSONDecoder().decode([ConvertJson].self, from: data!) {
+                        
+                        print(courcesFromJson)
+                        DispatchQueue.main.async {
+                            self.items = courcesFromJson
+                            tableView.reloadData()
+                        }
+                       
+                        
+                    }
                 }
                 
                 
@@ -42,11 +54,13 @@ getCources()
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
-    }
+        return  items.count   }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        let itemForCell = items[indexPath.row].title
+        cell?.textLabel?.text = itemForCell
         return cell!
     }
 
