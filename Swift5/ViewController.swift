470deece64956e54e735069112d391e800022095
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-17 14:50:16 +0200

    Initial Commit

---
+++ Swift5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5
+//
+//  Created by Сергей Голенко on 17.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
